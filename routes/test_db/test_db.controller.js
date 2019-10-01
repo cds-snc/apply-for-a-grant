fd4700716469fb9503c725e9c9d502452a681467
Tim Arney <tim@line37.com>
2019-10-01 11:54:02 -0400

    add db

--- routes/test_db/test_db.controller.js
+++ routes/test_db/test_db.controller.js
@@ -1,25 +1,32 @@
 const path = require('path')
 const { routeUtils } = require('./../../utils')
-// const dynamoose = require('dynamoose')
+const dynamoose = require('dynamoose')
 
 module.exports = app => {
   const name = 'test_db'
   const route = routeUtils.getRouteByName(name)
 
   routeUtils.addViewPath(app, path.join(__dirname, './'))
-  /*
-  dynamoose.AWS.config.update({})
 
-  const Submissions = dynamoose.model('Submissions', {
-    fullname: String,
+  dynamoose.AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: 'ca-central-1',
   })
 
+  
+  const Submissions = dynamoose.model('Submissions', {
+    id: Number,
+    fullname: String,
+  })
+  
+
   const entry = new Submissions({
-    fullname: 'Mr. Test',
+    id: 1,
+    fullname: 'Mr. Test 2',
   })
 
   entry.save()
-  */
 
   app.get(route.path, (req, res) => {
     res.render(name)
