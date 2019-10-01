c19b4925c38a406d441af4693692db22e0f28b4e
smcmurtry <smcmurtry@gmail.com>
2019-10-01 08:40:06 -0600

    started adding questions

--- routes/step-1/schema.js
+++ routes/step-1/schema.js
@@ -3,31 +3,10 @@
 const isValidDate = require('../../utils/').isValidDate
 
 const Schema = {
-  fullname: {
-    isLength: {
-      errorMessage: 'errors.fullname.length',
-      options: { min: 3, max: 200 },
-    },
-  },
   email: {
     isLength: {
       errorMessage: 'errors.email.length',
       options: { min: 3, max: 200 },
-    },
-  },
-  expiry: {
-    customSanitizer: {
-      options: value => {
-        // We want to remove any spaces, dash or underscores
-        return value ? value.replace(/[_]*/g, '') : value
-      },
-      errorMessage: 'errors.expiry.date.format',
-    },
-    custom: {
-      options: (value, { req }) => {
-        return isValidDate(value)
-      },
-      errorMessage: 'errors.expiry.date',
     },
   },
   send_notifications: {
