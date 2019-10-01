a942a9db6aa2796f418aa684c40e328664b0f481
Tim Arney <tim@line37.com>
2019-10-01 13:30:50 -0400

    move model

--- routes/test_db/test_db.controller.js
+++ routes/test_db/test_db.controller.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const { routeUtils } = require('./../../utils')
 const dynamoose = require('dynamoose')
+const { Submission } = require('../../db/model')
 
 module.exports = app => {
   const name = 'test_db'
@@ -8,22 +9,9 @@ module.exports = app => {
 
   routeUtils.addViewPath(app, path.join(__dirname, './'))
 
-  dynamoose.AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: 'ca-central-1',
-  })
-
-  
-  const Submissions = dynamoose.model('Submissions', {
-    id: Number,
-    fullname: String,
-  })
-  
-
-  const entry = new Submissions({
+  const entry = new Submission({
     id: 1,
-    fullname: 'Mr. Test 2',
+    fullname: 'Mr. Test 3',
   })
 
   entry.save()
